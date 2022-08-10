ae9c4aad666bc0a25c94e7c84f6a37cb8998a901
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-10 15:27:10 -0400

    Installed Bootstrap and Axios
    Included link to code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-   Hello
+      <div className="container">
+        <footer>
+          This code is 
+          <a
+            href="https://github.com/Nikoteenie/MyWeather-React-App.git"
+            rel="noreferrer"
+            target="_blank"
+          > open-sourced{" "}
+          </a> by <a
+            href="https://nikoteenie.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Nicole Brodkin
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
